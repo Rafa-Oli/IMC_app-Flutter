d639de264dff2aa845cc01cbb98ca38fc6b8006e
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-09-28 21:44:48 -0300

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora de IMC'),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: () {})],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(Icons.person_outline, size: 120.0, color: Colors.purple),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso(kg)",
+                labelStyle: TextStyle(color: Colors.purple)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.purple, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura(cm)",
+                labelStyle: TextStyle(color: Colors.purple)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.purple, fontSize: 25.0),
+          )
+        ],
+      ),
+    );
+  }
+}
