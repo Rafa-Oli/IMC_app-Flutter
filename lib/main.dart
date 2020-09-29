4c3b4b8528acc233b35b437478f772a4709ff631
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-09-28 22:22:16 -0300

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -18,27 +18,49 @@ class _HomeState extends State<Home> {
         actions: [IconButton(icon: Icon(Icons.refresh), onPressed: () {})],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(Icons.person_outline, size: 120.0, color: Colors.purple),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Peso(kg)",
-                labelStyle: TextStyle(color: Colors.purple)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.purple, fontSize: 25.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura(cm)",
-                labelStyle: TextStyle(color: Colors.purple)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.purple, fontSize: 25.0),
-          )
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(Icons.person_outline, size: 120.0, color: Colors.purple),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso(kg)",
+                  labelStyle: TextStyle(color: Colors.purple)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.purple, fontSize: 20.0),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura(cm)",
+                  labelStyle: TextStyle(color: Colors.purple)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.purple, fontSize: 20.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ),
+                  color: Colors.purple,
+                ),
+              ),
+            ),
+            Text(
+              'Info',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.purple, fontSize: 20.0),
+            )
+          ],
+        ),
       ),
     );
   }
